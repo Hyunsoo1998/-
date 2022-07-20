1b7e6e238701aabfd14b90645c682ccf3909e393
Hyunsoo1998 <98036819+Hyunsoo1998@users.noreply.github.com>
2022-07-20 17:29:41 +0900

    baekjoon_step_by_step

---
+++ baekjoon4344.py
@@ -0,0 +1,20 @@
+#test case 를 입력받을 변수 C로 입력받기.
+C = int(input())
+
+for i in range(C):
+    Score = list(map(int, input().split()))
+    Mean = sum(Score[1:])/Score[0]
+    cnt = 0
+    for j in range(1, len(Score)):
+        if Score[j] > Mean:
+            cnt += 1
+    percent = 100*cnt/Score[0]
+    print("{:.3f}%".format(round(percent,3)))
+
+
+
+
+
+
+
+
